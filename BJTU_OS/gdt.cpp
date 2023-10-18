01747870611b49e66cee6b494b18b0413cb31290
ls <3243106232@qq.com>
2023-10-19 01:33:13 +0800

    notes

--- BJTU_OS/gdt.cpp
+++ BJTU_OS/gdt.cpp
@@ -4,6 +4,8 @@ GlobalDescriptorTable::SegmentDescriptor::SegmentDescriptor(uint32_t base, uint3
 {
     uint8_t *target = (uint8_t *)this;
 
+    /*根据x86架构的要求，限制字段有不同的编码方式。
+    如果限制小于等于65536（64KB），则使用一种编码方式，否则使用另一种编码方式。*/
     if (limit <= 65536)
     {
         target[6] = 0x40;
@@ -42,6 +44,9 @@ uint32_t GlobalDescriptorTable::SegmentDescriptor::Base()
     result = (result << 8) + target[3];
     result = (result << 8) + target[2];
     return result;
+    /*包含了从段描述符中提取的完整基地址，这个基地址是一个32位无符号整数。
+    总之，这个函数用于从段描述符中提取并重构段的起始地址，以便后续在代码中使用。
+    这对于加载和使用段非常有用，例如在x86体系结构中用于确定代码段或数据段的物理地址*/
 }
 
 uint32_t GlobalDescriptorTable::SegmentDescriptor::Limit()
@@ -68,18 +73,34 @@ GlobalDescriptorTable::GlobalDescriptorTable()
     uint32_t i[2];
     i[1] = (uint32_t)this;
     i[0] = sizeof(GlobalDescriptorTable) << 16;
+    /*在x86体系结构中，GDTR（全局描述符表寄存器）的格式是一个48位的寄存器，
+    其中低16位存储 GDT 表的大小
+    而高32位存储 GDT 表的线性地址。
+    左移16位将 sizeof(GlobalDescriptorTable) 的值从字节转换为字（1字节 = 8位）*/
+
     __asm__ volatile("lgdt (%0)" : : "p"((uint8_t *)i + 2));
+    /*•	构建一个描述GDT的32位整数数组 i，其中包含 GDT 表的地址和大小。
+•	使用汇编内联指令 lgdt，将 GDT 表的地址和大小加载到全局描述符寄存器（GDTR）中。
+*/
 }
+
 GlobalDescriptorTable::~GlobalDescriptorTable()
 {
 }
 
 uint16_t GlobalDescriptorTable::CodeSegmentSelector()
 {
+    // 返回代码段和数据段的选择符
     return (uint8_t *)&codeSegmentSelector - (uint8_t *)this;
 }
 
 uint16_t GlobalDescriptorTable::DataSegmentSelector()
 {
+    // 返回代码段和数据段的选择符
     return (uint8_t *)&dataSegmentSelector - (uint8_t *)this;
 }
+/*这个偏移量通常用于在汇编代码中构建段选择器，以便将其加载到段寄存器中
+这是x86体系结构中设置段的一种典型方式。
+这个偏移量可以被用来设置段寄存器，如CS（代码段寄存器）或DS（数据段寄存器），
+以指向相应的段描述符，从而确定当前运行代码的权限和所处的段。
+在操作系统内核或底层系统编程中，这样的计算通常是必需的，以正确配置内存访问权限。*/
--- BJTU_OS/interrupts.cpp
+++ BJTU_OS/interrupts.cpp
@@ -7,11 +7,15 @@ InterruptManger::InterruptManger(GlobalDescriptorTable *gdt)
 {
     uint16_t CodeSegment = gdt->CodeSegmentSelector();
     const uint8_t IDT_INTERRUPT_GATE = 0xE;
+    //定义了中断门描述符的类型
 
     for (uint16_t i = 0; i < 256; i++)
     {
         SetInterruptDescriptorTableEntry(i, CodeSegment, &IgnoreInterruptRequest, 0, IDT_INTERRUPT_GATE);
     }
+    // 通过 for 循环，遍历256个中断号（0-255），对每个中断号都调用 SetInterruptDescriptorTableEntry 函数来设置
+    // IDT表的中断门描述符。
+    // 每个中断门描述符的中断处理函数都被设置为 IgnoreInterruptRequest，这个函数用于忽略中断请求，相当于占位符。
 
     SetInterruptDescriptorTableEntry(0x20, CodeSegment, &HandleInterruptRequest0x00, 0, IDT_INTERRUPT_GATE);
 
@@ -30,12 +34,21 @@ InterruptManger::InterruptManger(GlobalDescriptorTable *gdt)
     // 中断屏蔽码0闭1开
     picMasterData.Write(0x00);
     picSlaveData.Write(0x00);
+    /*发送初始化命令 0x11 给主PIC和从PIC，用于重新初始化这两个PIC。
+    配置主PIC和从PIC的中断向量起始地址。主PIC的起始地址为 0x20，从PIC的起始地址为 0x28。
+    通过 picMasterData.Write(0x04) 和 picSlaveData.Write(0x02) 来告诉PIC它们的中断控制关系（主从关系）。
+    最后，通过 picMasterData.Write(0x00) 和 picSlaveData.Write(0x00) 来设置中断屏蔽码，将所有中断都关闭。*/
 
-    InterruptsDescriptorTablePointer idt;
+    InterruptsDescriptorTablePointer idt;  //存储 IDT 表的指针和大小信息的数据结构
     idt.size = 256 *sizeof(GateDescriptor)-1;
     idt.base = (uint32_t)interruptDescriptorTable;
 
     __asm__ volatile("lidt %0": :"m" (idt));
+    /*内联汇编指令 lidt：
+         __asm__ volatile 是内联汇编的语法，用于在C++代码中插入汇编指令。
+        "lidt %0" 是 lidt 汇编指令的操作数，%0 表示要替换的第一个操作数。这里，%0 会被替换为 idt 结构体的地址。
+        冒号后面的内容是汇编指令的输入和输出参数。在这种情况下，没有输出参数，只有一个输入参数，即 idt 结构体的地址。
+        "m" 表示将 idt 结构体作为内存操作数传递给 lidt 指令*/
 }
 
 InterruptManger::~InterruptManger()
