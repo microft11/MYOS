e037bbedc99972f360e8718b199c00189e371537
ls <3243106232@qq.com>
2023-12-11 00:46:09 +0800

    riscv-gdp

--- rCore_OS/ch1.LibOS/os/src/main.rs
+++ rCore_OS/ch1.LibOS/os/src/main.rs
@@ -1,6 +1,10 @@
 #![no_std]
+#![no_main]
 mod lang_item;
 
-fn main() {
-    // println!("Hello, world!");
-}
+use core::arch::global_asm;
+global_asm!(include_str!("entry.asm"));
+
+// fn main() {
+//     // println!("Hello, world!");
+// }
