0d71ccda87398eec62822d9a93a0b9e265f0990f
ls <3243106232@qq.com>
2023-12-11 13:27:57 +0800

    libos add sbi with wrong

--- rCore_OS/ch1.LibOS/os/src/main.rs
+++ rCore_OS/ch1.LibOS/os/src/main.rs
@@ -1,9 +1,32 @@
 #![no_std]
 #![no_main]
-mod lang_item;
+#![feature(panic_info_message)]
+
+#[macro_use]
+mod console;
+mod lang_items;
+mod sbi;
 
 use core::arch::global_asm;
 global_asm!(include_str!("entry.asm"));
+
+fn clear_bss() {
+    
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    } // 使用 range（sbss 到 ebss）迭代 BSS 段内存范围
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
+}
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("Hello, world!");
+    panic!("Shutdown machine!");
+}
 
 // fn main() {
 //     // println!("Hello, world!");
