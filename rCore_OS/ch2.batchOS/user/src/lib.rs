fb7f925a915e878ac9577e7a6e355895bfe53937
ls <3243106232@qq.com>
2023-12-13 23:24:44 +0800

    usr note

--- rCore_OS/ch2.batchOS/user/src/lib.rs
+++ rCore_OS/ch2.batchOS/user/src/lib.rs
@@ -1,5 +1,5 @@
 #![no_std]
-#![feature(linkage)]
+#![feature(linkage)]   // to suppot weak link
 #![feature(panic_info_message)]
 
 #[macro_use]
@@ -7,7 +7,7 @@ pub mod console;
 mod lang_items;
 mod syscall;
 
-#[no_mangle]
+#[no_mangle]   // #[no_mangle] 告诉编译器不要修改或者改变特定的函数或变量的名称
 #[link_section = ".text.entry"]
 pub extern "C" fn _start() -> ! {
     clear_bss();
@@ -15,8 +15,8 @@ pub extern "C" fn _start() -> ! {
     panic!("unreachable after sys_exit!");
 }
 
-#[linkage = "weak"]
-#[no_mangle]
+#[linkage = "weak"]  // we have two main . weak main meas orther's main is 'main'
+#[no_mangle]  
 fn main() -> i32 {
     panic!("Cannot find main!");
 }
--- rCore_OS/ch2.batchOS/user/src/syscall.rs
+++ rCore_OS/ch2.batchOS/user/src/syscall.rs
@@ -4,7 +4,7 @@ const SYSCALL_WRITE: usize = 64;
 const SYSCALL_EXIT: usize = 93;
 
 fn syscall(id: usize, args: [usize; 3]) -> isize {
-    let mut ret: isize;
+    let mut ret: isize;  // 相比global_asm!,asm!宏可以获取上下文中的变量信息并允许嵌入的汇编代码对这些变量进行操作
     unsafe {
         asm!(
             "ecall",
@@ -17,10 +17,20 @@ fn syscall(id: usize, args: [usize; 3]) -> isize {
     ret
 }
 
+/// 功能：将内存中缓冲区中的数据写入文件。
+/// 参数：`fd` 表示待写入文件的文件描述符；
+///      `buf` 表示内存中缓冲区的起始地址；
+///      `len` 表示内存中缓冲区的长度。
+/// 返回值：返回成功写入的长度。
+/// syscall ID：64
 pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
     syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
 }
 
+/// 功能：退出应用程序并将返回值告知批处理系统。
+/// 参数：`exit_code` 表示应用程序的返回值。
+/// 返回值：该系统调用不应该返回。
+/// syscall ID：93
 pub fn sys_exit(exit_code: i32) -> isize {
     syscall(SYSCALL_EXIT, [exit_code as usize, 0, 0])
 }
